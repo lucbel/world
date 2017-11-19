0485573e16e9fd98d060abe5cf8c2137384d950f
Rodrigo <rodrigo@themoatcrew.com>
2017-11-19 18:08:14 +0100

    create basic application

---
+++ src/main/java/com/werlabs/world/MainConfiguration.java
@@ -0,0 +1,13 @@
+package com.werlabs.world;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rodrigo on 18/11/17.
+ */
+
+@SpringBootApplication
+public class MainConfiguration {
+
+}
