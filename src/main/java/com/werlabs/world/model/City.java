1d81e5e7ad8d2033b75447ff8e3fb728e04f983c
Rodrigo <rodrigo@themoatcrew.com>
2017-11-19 18:03:08 +0100

    add city model

---
+++ src/main/java/com/werlabs/world/model/City.java
@@ -0,0 +1,56 @@
+package com.werlabs.world.model;
+
+import javax.persistence.Table;
+
+/**
+ * Created by rodrigo on 18/11/17.
+ */
+@Table(name = "city")
+public class City {
+
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private String population;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+}
